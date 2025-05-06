3d06fe1d4fd57ac2c44eb860c73020b2388948d2
Chiranjeev-18 <123480005+Chiranjeev-18@users.noreply.github.com>
2025-05-06 22:19:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("who are you");
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+  
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return;
+  
+    const li = document.createElement("li");
+    li.textContent = taskText;
+  
+    li.addEventListener("click", function () {
+      li.classList.toggle("completed");
+    });
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete-btn";
+    deleteBtn.onclick = function () {
+      taskList.removeChild(li);
+    };
+  
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+  
